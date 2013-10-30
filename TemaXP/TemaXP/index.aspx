461d5e9187ef61d78fd7dbb5397bdd65670d090e
Ronnie Hemmingsen <ronnie.hemmingsen@gmail.com>
2013-10-30 15:36:30 +0100

    skeletkonstruktion

---
+++ TemaXP/TemaXP/index.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="false" CodeBehind="index.aspx.cs" Inherits="TemaXP.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <p>test</p>
+        <asp:Button ID="BtnTest" runat="server" Text="Testknap" OnClick="BtnTest_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ TemaXP/TemaXP/index.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TemaXP
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BtnTest.Text = "refreshed";
+            }
+        }
+
+        protected void BtnTest_Click(object sender, EventArgs e)
+        {
+            click();
+        }
+
+        private void click()
+        {
+            BtnTest.Text = "Clicked";
+        }
+    }
+}
