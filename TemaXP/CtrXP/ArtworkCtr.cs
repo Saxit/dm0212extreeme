d5ee8ffab748c3522fc699555839d3016b19f8b1
TokeOlsen <saxit_devil@hotmail.com>
2013-11-04 12:02:38 +0100

    Forsøgt at få forbindelse til databasen for Artwork

--- TemaXP/CtrXP/ArtworkCtr.cs
+++ TemaXP/CtrXP/ArtworkCtr.cs
@@ -3,19 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ModelXP;
+using ModelXP.Models;
+using System.Data.Entity;
 
 namespace CtrXP
 {
     public class ArtworkCtr
     {
+        private dmaa0212a_6Context dbContext;
         public ArtworkCtr()
         {
+            dbContext = new dmaa0212a_6Context();
         }
 
         public void CreateArtwork(string nameOfArt, string nameOfArtist, string yearOfArt, double heightOfArt, double widthOfArt, string location, string urlOfImage)
         {
-            Artwork aw = new Artwork(nameOfArt, nameOfArtist, yearOfArt, heightOfArt, widthOfArt, location, urlOfImage);
+
+            Artwork aw = new Artwork()
+            {
+                artName = nameOfArt,
+                yearCreated = yearOfArt,
+                height = (decimal)heightOfArt,
+                width = (decimal)widthOfArt,
+                location = location,
+                imagePath = urlOfImage
+                
+            };
+            
+            dbContext.Artworks.Add(aw);
+            dbContext.SaveChanges();
 
         }
     }
--- TemaXP/ModelXP/Models/Artwork.cs
+++ TemaXP/ModelXP/Models/Artwork.cs
@@ -7,12 +7,13 @@ namespace ModelXP.Models
     {
         public Artwork()
         {
+            var db = new dmaa0212a_6Context();
             this.Auctions = new List<Auction>();
         }
 
         public int id { get; set; }
         public string artName { get; set; }
-        public Nullable<int> yearCreated { get; set; }
+        public string yearCreated { get; set; }
         public Nullable<decimal> height { get; set; }
         public Nullable<decimal> width { get; set; }
         public string location { get; set; }
